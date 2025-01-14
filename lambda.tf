4b74155cc08676e6dc0e9599213b62457116078a
Hasan Abdirahman <hasanabdirahman.ao@gmail.com>
2025-01-14 13:16:49 -0500

    last change

--- lambda.tf
+++ lambda.tf
@@ -25,7 +25,7 @@ resource "aws_lambda_function" "lambda" {
   handler          = local.handler
   source_code_hash = data.archive_file.init.output_base64sha256
   package_type     = local.package_type
-  role             = aws_iam_role.iam_for_lambda.id
+  role             = aws_iam_role.iam_for_lambda.arn
   runtime          = "nodejs18.x"
   tags = {
     "lambda-console:blueprint" = "hello-world"
@@ -34,4 +34,12 @@ resource "aws_lambda_function" "lambda" {
     log_format = "Text"
     log_group  = "/aws/lambda/helloWorld"
   }
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.image_bucket.arn
 }
